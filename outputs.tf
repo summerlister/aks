2195100d446cf01e407485a030717218fb38d30c
Summer Lister <summerlister@microsoft.com>
2023-12-11 12:23:44 -0500

    remove output

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-//cluster ID
-
-output "cluster_id" {
-  value = azurerm_kubernetes_cluster.cluster.id
-}
-
-output "kube_admin_config" {
-  value = azurerm_kubernetes_cluster.cluster.kube_admin_config
-}
